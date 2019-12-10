43a43eb0a681c09768d12fe4e2505a94bd1fe5c7
abhishek8899 <abhishek2mx3@gmail.com>
2019-12-10 13:27:15 +0530

    TextRank

---
+++ TextRank/main.py
@@ -0,0 +1,28 @@
+from keyword_extractor import KeywordExtractor
+import argparse
+#import sys
+#sys.excepthook = sys.__excepthook__
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--word2vec", default=None, help="path to word2vec pre-trained embeddings")
+ap.add_argument("--data", required=True, help="path to file from which keywords are to be extracted")
+
+args = ap.parse_args()
+
+with open(args.data, 'r') as data_file:
+    lines = data_file.readlines()
+
+#print(lines)
+
+extractor = KeywordExtractor(word2vec=args.word2vec)
+
+#print(extractor)
+
+
+for text in lines:
+    keywords = extractor.extract(text, ratio=0.2, split=True,scores=True)
+    #print(keywords)
+    for keyword in keywords:
+        print(keyword)
+
