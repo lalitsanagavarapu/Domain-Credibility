5b964f39389c5ff83f5e5175d8a2da0557ffdc8c
abhishek8899 <avi27.999@gmail.com>
2019-12-19 16:55:19 +0530

    Plugin

--- API_PLUGIN/Plugin_API/app.js
+++ API_PLUGIN/Plugin_API/app.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "127.0.0.1"); // update to match the domain you will make the request from
+  //res.header("Access-Control-Allow-Origin", "127.0.0.1");
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
